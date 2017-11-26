8cfca113f17bdfeab1ef336a4f460c1c67ee3d7e
Li_Szu <henry8209@gmail.com>
2017-11-27 00:31:05 +0800

    add2
    add4

---
+++ web/test.php
@@ -0,0 +1,7 @@
+<?php
+$count = file_get_contents("http://140.117.6.187/Analysis/FunctionDisplay/linebot_get_point.php");
+//$add = file_get_contents("http://140.117.6.187/Analysis/FunctionDisplay/linebot_add_point.php");
+//$ex = file_get_contents("http://140.117.6.187/Analysis/FunctionDisplay/linebot_change_point.php");
+echo $count;
+   
+?>            

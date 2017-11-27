2b55fbb3b359c5e56a2ba08347c407777bd30257
SHanDesign1993 <32634210+SHanDesign1993@users.noreply.github.com>
2017-11-27 10:00:15 +0800

    adas
    ad

--- web/index.php
+++ web/index.php
@@ -31,21 +31,16 @@ foreach ($client->parseEvents() as $event) {
             switch ($message['type']) {
                 case 'text':
                 	$m_message = $message['text'];
-                    $emoticon=unichr(0x1000B6);
-                    $r_message='嗨！毛毛'.$emoticon.'~'.urlencode('\n').'你是來領取點數的嗎？要跟我說通關密語哦';
+                    $r_message='嗨！毛毛'.unichr(0x100037).'~'.urlencode(\n).'你是來領取點數的嗎？要跟我說通關密語哦';
                         if( strpos( $message['text'], '點數' ) !== false || strpos( $message['text'], '查' ) !== false){
                             $count = file_get_contents("http://140.117.6.187/Analysis/FunctionDisplay/linebot_get_point.php");
-                            $bin = hex2bin(str_repeat('0', 8 - strlen('1000B6')) . '1000B6');
-                            $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                            $r_message='毛毛現在總共有 '.$count.' 點了耶耶!'.$emoticon;
+                            $r_message='毛毛現在總共有 '.$count.' 點!!!'.unichr(0x1000B6);
                             if($count>=3){
-                              $bin = hex2bin(str_repeat('0', 8 - strlen('100091')) . '100091');
-                              $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                              $r_message.=urlencode('\n').'好誇喔喔喔'.$emoticon;
+                              $r_message.=urlencode(\n).'好誇喔喔喔'.unichr(0x100091);
+                            }else if($count==0){
+                              $r_message.=urlencode(\n).'恩...多笑一點吧！'.unichr(0x10008E);
                             }else{
-                              $bin = hex2bin(str_repeat('0', 8 - strlen('10008A')) . '10008A');
-                              $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                              $r_message.=urlencode('\n').'繼續加油囉'.$emoticon;
+                              $r_message.=urlencode(\n).'繼續加油囉'.unichr(0x10008A);
                             }
                             
                         }
@@ -54,13 +49,9 @@ foreach ($client->parseEvents() as $event) {
                             $add = file_get_contents("http://140.117.6.187/Analysis/FunctionDisplay/linebot_add_point.php");
                             $count = file_get_contents("http://140.117.6.187/Analysis/FunctionDisplay/linebot_get_point.php");
                             if($add=='ok'){
-                              $bin = hex2bin(str_repeat('0', 8 - strlen('100022')) . '100022');
-                              $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                              $r_message='毛寶寶爭氣的獲得了1點&#100037'.urlencode('\n').'總共有 '.$count.'點了哦嘿嘿'.$emoticon;
+                              $r_message='毛寶寶爭氣的獲得了1點&#100037'.urlencode(\n).'總共有 '.$count.'點了哦嘿嘿'.unichr(0x100022);
                             }else{
-                              $bin = hex2bin(str_repeat('0', 8 - strlen('10000')) . '10000');
-                              $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                              $r_message='毛毛今天拿過點數了喔！'.urlencode('\n').$emoticon;
+                              $r_message='毛毛今天拿過點數了喔！'.urlencode(\n).unichr(0x10000E);
                             }
                         }
 

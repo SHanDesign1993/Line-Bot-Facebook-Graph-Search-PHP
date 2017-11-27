9500fd91c20eb9292d1905e899a47e6ed8be6297
SHanDesign1993 <32634210+SHanDesign1993@users.noreply.github.com>
2017-11-27 09:05:58 +0800

    eno
    emo

--- web/index.php
+++ web/index.php
@@ -31,7 +31,7 @@ foreach ($client->parseEvents() as $event) {
             switch ($message['type']) {
                 case 'text':
                 	$m_message = $message['text'];
-                    $r_message='嗨！毛毛&#100008~\n你是來領取點數的嗎？要跟我說通關密語哦';
+                    $r_message='嗨！毛毛{GetEmoji('100008')}~\n你是來領取點數的嗎？要跟我說通關密語哦';
                         if( strpos( $message['text'], '點數' ) !== false || strpos( $message['text'], '查' ) !== false){
                             $count = file_get_contents("http://140.117.6.187/Analysis/FunctionDisplay/linebot_get_point.php");
                             $r_message='毛毛現在總共有 '.$count.' 點了耶耶!&#1000B6';
@@ -81,3 +81,9 @@ foreach ($client->parseEvents() as $event) {
             break;
     }
 };
+
+function GetEmoji($code){
+    $bin = hex2bin(str_repeat('0', 8 - strlen($code)) . $code);
+    $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
+    return $emoticon;
+}

a27ab1b0300901e4cd9f41e4ee00da968ed11ec1
SHanDesign1993 <32634210+SHanDesign1993@users.noreply.github.com>
2017-11-27 09:43:13 +0800

    asd
    asda

--- web/index.php
+++ web/index.php
@@ -31,21 +31,21 @@ foreach ($client->parseEvents() as $event) {
             switch ($message['type']) {
                 case 'text':
                 	$m_message = $message['text'];
-                    $emoticon='...';
-                    $r_message='嗨！毛毛'.$emoticon.'~\n你是來領取點數的嗎？要跟我說通關密語哦';
+                    $emoticon=unichr(0x1000B6);
+                    $r_message=urlencode('嗨！毛毛'.$emoticon.'~\n你是來領取點數的嗎？要跟我說通關密語哦');
                         if( strpos( $message['text'], '點數' ) !== false || strpos( $message['text'], '查' ) !== false){
                             $count = file_get_contents("http://140.117.6.187/Analysis/FunctionDisplay/linebot_get_point.php");
                             $bin = hex2bin(str_repeat('0', 8 - strlen('1000B6')) . '1000B6');
                             $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                            $r_message='毛毛現在總共有 '.$count.' 點了耶耶!'.$emoticon;
+                            $r_message=urlencode('毛毛現在總共有 '.$count.' 點了耶耶!'.$emoticon);
                             if($count>=3){
                               $bin = hex2bin(str_repeat('0', 8 - strlen('100091')) . '100091');
                               $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                              $r_message.='\n好誇喔喔喔'.$emoticon;
+                              $r_message.=urlencode('\n好誇喔喔喔'.$emoticon);
                             }else{
                               $bin = hex2bin(str_repeat('0', 8 - strlen('10008A')) . '10008A');
                               $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                              $r_message.='\n繼續加油囉'.$emoticon;
+                              $r_message.=urlencode('\n繼續加油囉'.$emoticon);
                             }
                             
                         }
@@ -56,11 +56,11 @@ foreach ($client->parseEvents() as $event) {
                             if($add=='ok'){
                               $bin = hex2bin(str_repeat('0', 8 - strlen('100022')) . '100022');
                               $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                              $r_message='毛寶寶爭氣的獲得了1點&#100037\n總共有 '.$count.'點了哦嘿嘿'.$emoticon;
+                              $r_message=urlencode('毛寶寶爭氣的獲得了1點&#100037\n總共有 '.$count.'點了哦嘿嘿'.$emoticon);
                             }else{
                               $bin = hex2bin(str_repeat('0', 8 - strlen('10000')) . '10000');
                               $emoticon =  mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-                              $r_message='毛毛今天拿過點數了喔！\n'.$emoticon;
+                              $r_message=urlencode('毛毛今天拿過點數了喔！\n'.$emoticon);
                             }
                         }
 
@@ -92,4 +92,8 @@ foreach ($client->parseEvents() as $event) {
             break;
     }
 };
+
+function unichr($i) {
+    return iconv('UCS-4LE', 'UTF-8', pack('V', $i));
+}
 ?>

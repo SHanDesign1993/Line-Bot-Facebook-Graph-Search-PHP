56bc3b0bd768cb898f3ea519269681c387c69372
SHanDesign1993 <32634210+SHanDesign1993@users.noreply.github.com>
2017-12-20 23:13:52 +0800

    c

--- web/index.php
+++ web/index.php
@@ -269,7 +269,8 @@ foreach ($client->parseEvents() as $event) {
                     $lat=0;
                     $lon=0;
                     $FB_URL="https://graph.facebook.com/v2.9/search?q=%27restaurant%27&type=place&center={$lat},{$lon}&distance=500&locale=zh-TW&fields=location,name,overall_star_rating,rating_count,phone,link,price_range,category_list,%20hours&access_token=EAACEdEose0cBAHlBy6z7LHAIIkMlDaNZCzjmS7DEaLsrWhQGKGW02skS1Uj3acy9kTarov3qsORTvQ1trj210AKzZAG4cfschKL5PNg96gSz0SHhZB3o35Teo4CKgXuOGyoEVVUGTJ8OJPOzIsJAfNtr1pW4EhzG7vhsqQRGpWUZAMCDTnihsFTdIqPjRzcZD&limit=5";
-                    PushFBFood($to_me,$FB_URL,$channelAccessToken);
+                    PushMessage($to_me,$message,$channelAccessToken);
+                    //PushFBFood($to_me,$FB_URL,$channelAccessToken);
                 break;
                 
             }

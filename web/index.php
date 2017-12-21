12e69404e6051a68790024bebf73056461608c3f
SHanDesign1993 <32634210+SHanDesign1993@users.noreply.github.com>
2017-12-21 08:20:44 +0800

    c

--- web/index.php
+++ web/index.php
@@ -267,17 +267,11 @@ foreach ($client->parseEvents() as $event) {
                 case 'location';
                     $lat=$message['latitude'];
                     $lon=$message['longitude'];
-                    $FB_URL="https://graph.facebook.com/v2.9/search?q=%27restaurant%27&type=place&center={$lat},{$lon}&distance=500&locale=zh-TW&fields=location,name,overall_star_rating,rating_count,phone,link,price_range,category_list,%20hours&access_token=EAACEdEose0cBAHlBy6z7LHAIIkMlDaNZCzjmS7DEaLsrWhQGKGW02skS1Uj3acy9kTarov3qsORTvQ1trj210AKzZAG4cfschKL5PNg96gSz0SHhZB3o35Teo4CKgXuOGyoEVVUGTJ8OJPOzIsJAfNtr1pW4EhzG7vhsqQRGpWUZAMCDTnihsFTdIqPjRzcZD&limit=5";
-                   $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $FB_URL
-                            )
-                        )
-                        ));
-                    //PushFBFood($to_me,$FB_URL,$channelAccessToken);
+                    $fb =  file_get_contents('http://140.117.6.187/web-s/access_key.json');
+                    $access_key = json_decode($fb)['key'];
+                    $FB_URL="https://graph.facebook.com/v2.9/search?q=%27restaurant%27&type=place&center={$lat},{$lon}&distance=500&locale=zh-TW&fields=location,name,overall_star_rating,rating_count,phone,link,price_range,category_list,%20hours&access_token={$access_key}&limit=5";
+                   
+                    PushFBFood($to_me,$FB_URL,$channelAccessToken);
                     
                 break;
                 
